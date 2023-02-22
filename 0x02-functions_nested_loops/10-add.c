b5d1ea7e136052c850072b6e8531e2cde60fe5aa
web-solution10 <ammarelbadry1@gmail.com>
2023-02-21 17:26:57 -0800

    some tasks upload

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * add - f
+ *
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: the sum
+ */
+
+int add(int a, int b);
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - test
+ *
+ * @n: input
+ *
+ * Return: no return
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+		putchar('\n');
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+		putchar('\n');
+	}
+}
