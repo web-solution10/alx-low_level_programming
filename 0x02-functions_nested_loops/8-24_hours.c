cd5120b4a8411d67db2f6bc83c7880b11ad6f679
web-solution10 <ammarelbadry1@gmail.com>
2023-02-21 16:18:06 -0800

    task 8 upload

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - just a fuction
+ *
+ * Return: nothing
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i < 51; i++)
+	{
+		for (j = 48; j < 52; j++)
+		{
+			for (k = 48; k < 54; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(58);
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
+
