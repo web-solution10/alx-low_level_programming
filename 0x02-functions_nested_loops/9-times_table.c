624c3ff7b373f8598e1591a8da8106d8917b77f5
web-solution10 <ammarelbadry1@gmail.com>
2023-02-21 17:39:16 -0800

    task 9 upload

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times table
+ *
+ * Return: there is no return
+ */
+
+void times_table(void)
+{
+	int i, j, ij;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(48);
+		for (j = 1; j <= 9; j++)
+		{
+			ij = i * j;
+			_putchar(44);
+			_putchar(32);
+			if (ij <= 9)
+			{
+				_putchar(32);
+				_putchar(ij + 48);
+			}
+			else
+			{
+				_putchar((ij / 10) + 48);
+				_putchar((ij % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
 
