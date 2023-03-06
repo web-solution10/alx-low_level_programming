2c5abaf520a181f88297e77820742f484cb825e3
web-solution10 <ammarelbadry1@gmail.com>
2023-03-07 00:05:45 +0200

    tasks upload

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,6 +5,7 @@
  * @dest: memory area to be copied to
  * @src: memory area to be copied from
  * @n: number of src bytes to be copied
+ * Return: a pointer to dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
