8e22d9690a17488ee17aaed684f914a06492845c
web-solution10 <ammarelbadry1@gmail.com>
2023-03-11 01:54:10 +0200

    task 0 upload

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: the count of the arguements
+ * @argv: array of pointers to the string arguments
+ * Return: return 0
+ */
+
+int main(int argc __attribute__ ((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
